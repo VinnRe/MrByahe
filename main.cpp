d416c2e1e4cc16caa0a40c1de3b88fd85f689f84
VinnRe <22-03804@g.batstate-u.edu.ph>
2023-07-04 17:28:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 using namespace std;
 
+class busData {
+    // Stores the bus data
+    // Plate No.
+    // Capacity of Bus
+    // Destination of Bus
+    // Time of ETD (Expected Time of Departure)
+    // Bus Schedules
+};
+
+class termManager{
+    // Sets how many buses currently in the terminal
+    // Records if a passenger boards on a specific bus
+    // Records if a passenger disembark
+    // Get the passenger count for a specific bus
+    // Get total passengers in all bus
+    // Get The Buses' Max Passengers
+    // Add Buses
+    // Remove Bus if full and have already left
+    // Add Bus Schedules
+    // Color Coding: Green if not full, Orange if half-way full, and Red if full
+    // Display Terminal Map
+    // Display Which buses are FULL, Half-Way, or Not Full
+    // Display ETD (Expected Time of Departure) of busses
+
+    // OPTIONAL
+    // Sorts Bus depending on which location they're going
+    // Display Fares
+
+};
+
 int main(){
     return 0;
 }
